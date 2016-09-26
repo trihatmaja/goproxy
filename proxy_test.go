8003e0f83ab474b339d8b6252bae111e895ab1f3
Wicaksono Trihatmaja <trihatmaja@gmail.com>
2016-09-26 14:11:25 +0700

    messing up with proxy test

--- proxy_test.go
+++ proxy_test.go
@@ -26,6 +26,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var hahahahahahahahahaha string
+
 var acceptAllCerts = &tls.Config{InsecureSkipVerify: true}
 
 var noProxyClient = &http.Client{Transport: &http.Transport{TLSClientConfig: acceptAllCerts}}
