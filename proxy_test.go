7045b1c58cb80e3ae187ab9e94745ba52901c31c
Wicaksono Trihatmaja <trihatmaja@gmail.com>
2016-09-26 14:09:04 +0700

    fixing proxy test

--- proxy_test.go
+++ proxy_test.go
@@ -26,8 +26,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var tahahslh string
-
 var acceptAllCerts = &tls.Config{InsecureSkipVerify: true}
 
 var noProxyClient = &http.Client{Transport: &http.Transport{TLSClientConfig: acceptAllCerts}}
